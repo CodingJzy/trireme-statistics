99d33285985b35478be346c73a90b9881adcbbc5
Sibi Chakravarthy <sibi@aporeto.com>
2017-11-02 02:47:25 -0500

    New: Using maps improves performance

--- graph/server/types.go
+++ graph/server/types.go
@@ -8,23 +8,21 @@ import (
 
 // GraphData is the struct that holds the json format required for graph to generate nodes and link
 type GraphData struct {
-	Nodes []Nodes `json:"nodes"`
-	Links []Links `json:"links"`
+	Nodes []Node `json:"nodes"`
+	Links []Link `json:"links"`
 }
 
-// Nodes which holds pu information
-type Nodes struct {
+// Node which holds pu information
+type Node struct {
 	Time      time.Time `json:"time"`
 	ContextID string    `json:"id"`
 	PodName   string    `json:"name"`
 	IPAddress string    `json:"ipaddress"`
-	IPIDHash  string    `json:"ipidhash"`
 	Namespace string    `json:"namespace"`
-	Delete    bool      `json:"delete"`
 }
 
-// Links which holds the links between pu's
-type Links struct {
+// Link which holds the links between pu's
+type Link struct {
 	Time      time.Time `json:"time"`
 	Source    string    `json:"source"`
 	Target    string    `json:"target"`
@@ -37,9 +35,11 @@ type Graph struct {
 	jsonData   *GraphData
 	httpClient *influxdb.Influxdb
 	dbname     string
-	nodes      []Nodes
-	links      []Links
-	nodesChan  chan []Nodes
-	linksChan  chan []Links
+	nodes      []Node
+	links      []Link
+	nodesChan  chan []Node
+	linksChan  chan []Link
+	nodeMap    map[string]*Node
+	linkMap    map[string]*Link
 	tagValue   string
 }
--- version/version.go
+++ version/version.go
@@ -4,4 +4,4 @@ package version
 const VERSION = "0.11"
 
 // REVISION is the revision of trireme-statistics
-const REVISION = "866574628c333acc45c778091e30a32052444f4c"
+const REVISION = "fe563e41c2250b7253d5ff2f49578aac6e27af57"
